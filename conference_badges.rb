37469269f2c46d362c74b216b0bb7245eb478e4e
Elliott Vargo <vargoelliott@gmail.com>
2021-01-17 02:21:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+# Write your code here.\
+name_array ["Ed", "Joe", "Bloe", "James", "Fiona", "Tiffany", ]
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+
+end
+
+def assign_rooms()
+end
+
+
+def printer()
+end
